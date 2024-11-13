b52862fed13a5df1fd37bba378edc5f1dfdca28d
Sean Murphy <sean@iamseanmurphy.com>
2024-11-13 05:28:41 +0000

    1.4.3

--- classic-editor-and-classic-widgets/classic-editor-and-classic-widgets.php
+++ classic-editor-and-classic-widgets/classic-editor-and-classic-widgets.php
@@ -8,14 +8,14 @@
  * License: GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: classic-editor-and-classic-widgets
- * Version: 1.4.2
+ * Version: 1.4.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CEW_VERSION', '1.4.2' );
+define( 'CEW_VERSION', '1.4.3' );
 define( 'CEW_FILE', __FILE__ );
 define( 'CEW_PATH', dirname( CEW_FILE ) );
 define( 'CEW_INCLUDES', CEW_PATH . '/includes' );
